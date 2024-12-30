91ec0b83c18138fedd5633e280ad0ae44821dc98
Hera <mostafizhera@gmail.com>
2024-12-30 16:23:49 +0600

    added the react ratting pakege

--- src/Pages/HomePage/Testimonials/Testmonials.jsx
+++ src/Pages/HomePage/Testimonials/Testmonials.jsx
@@ -1,56 +1,53 @@
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-
-// import required modules
-import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
-
+import { Navigation } from 'swiper/modules';
 import SectionTitle from '../Categorys/SectionTitle';
+import { useEffect, useState } from 'react';
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css'
 
 
 export default function Testmonials() {
+
+    const [review, setReview] = useState([])
+
+    useEffect(() => {
+        fetch('review.json')
+            .then(response => response.json())
+            .then(data => setReview(data))
+    }, [])
+
+
+
     return (
         <>
-            <div>
+            <div className=' mt-20'>
                 <SectionTitle
-                    subHeading={"What Our Clint Say"}
-                    heading={"testimonials"}
+                    subHeading={" What Our Clint Say "}
+                    heading={" testimonials "}
                 />
-
-                <Swiper
-                    cssMode={true}
-                    navigation={true}
-                    pagination={true}
-                    autoplay={true}
-                    infinityLoop={true}
-                    mousewheel={true}
-                    interval={3000}
-                    keyboard={true}
-                    modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-                    className="mySwiper"
-                >
-                    <SwiperSlide>
-                        <h2>this is main page here now</h2>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <h2>this is main page here now</h2>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <h2>this is main page here now</h2>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <h2>this is main page here now</h2>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <h2>this is main page here now</h2>
-                    </SwiperSlide>
+                <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                    {
+                        review.map((item, index) => (
+                            <SwiperSlide key={index}>
+                                <div className="text-center w-6/12 mx-auto my-20">
+                                    <p className=' text-2xl font-bold text-blue-600'>{item.name}</p>
+                                    <p className=' text-center my-5 flex justify-center'>
+                                        <Rating
+                                            style={{ maxWidth: 180 }}
+                                            value={item.rating}
+                                            readOnly
+                                        />
+                                    </p>
+                                    <span className=' text-md'>{item.details}</span>
+                                </div>
+                            </SwiperSlide>
+                        ))
+                    }
                 </Swiper>
-
             </div>
         </>
     )
