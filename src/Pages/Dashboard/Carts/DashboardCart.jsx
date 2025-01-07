a68d7bdd200c0c4a4daf0b4d720e35e32389ad54
Hera <mostafizhera@gmail.com>
2025-01-07 15:04:35 +0600

    deleted the cart data

--- src/Pages/Dashboard/Carts/DashboardCart.jsx
+++ src/Pages/Dashboard/Carts/DashboardCart.jsx
@@ -1,7 +1,89 @@
 import React from 'react'
+import useAuth from '../../../Hooks/useAuth/useAuth'
+import useCart from '../../../Hooks/useCart'
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
 
 export default function DashboardCart() {
+
+  const [cart, refetch] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  const axiosSecure = useAxiosSecure();
+
+  const handleDeleteCart = id => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/carts/${id}`)
+          .then(res => {
+            if (res.data.deletedCount > 0) {
+              refetch();
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+            }
+
+          })
+      }
+    });
+
+  }
+
   return (
-    <div>DashboardCart</div>
+    <>
+      <div className="overflow-x-auto w-full px-10">
+        <h2 className=' text-2xl font-semibold'>Total Items: ({cart.length})</h2>
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Items Image</th>
+              <th>Items Name</th>
+              <th>Items Price</th>
+              <th>Items Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {
+              cart.map((item, index) => (
+                <tr>
+                  <td>{index + 1}
+                  </td>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img
+                            src={item.image}
+                            alt="Avatar Tailwind CSS Component" />
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{item.name}
+                  </td>
+                  <td>
+                    {item.price}
+                  </td>
+                  <td><button onClick={() => handleDeleteCart(item._id)} className=' btn btn-error'>Delete</button></td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+        <h2 className=' text-2xl font-semibold'>Total price = ${totalPrice}</h2>
+      </div>
+    </>
   )
 }
--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -4,48 +4,49 @@ import { FaBars, FaCartArrowDown, FaShoppingBag, FaSwatchbook } from 'react-icon
 import { GrContactInfo } from 'react-icons/gr'
 import { HiUserGroup } from 'react-icons/hi'
 import { IoHomeSharp } from 'react-icons/io5'
+import { MdContactPhone } from 'react-icons/md'
 import { NavLink, Outlet } from 'react-router-dom'
 
 export default function Dashboard() {
   return (
     <>
       <div className=' container mx-auto flex gap-10'>
-        <div className=' w-80 bg-[#D6904B] text-gray-800 p-5 h-screen'>
-        <h2 className=' text-3xl font-semibold text-center mb-5'>Admin Panel</h2>
+        <div className=' w-80 bg-[#05030f] text-gray-800 p-5 h-screen'>
+          <h2 className=' text-3xl font-semibold text-center mb-5 text-slate-400'>Admin Panel</h2>
           <ul className=' flex flex-col gap-3'>
-            <li className=' flex gap-3 justify-start btn btn-info text-left items-center'><IoHomeSharp />
+            <li className=' flex gap-3 justify-start btn  text-left items-center'><IoHomeSharp />
               <NavLink to="/home">User Home</NavLink>
             </li>
-            <li  className=' flex gap-3 justify-start btn btn-info text-left items-center'>
-            <HiUserGroup />
-              <NavLink>Reservation</NavLink>
+            <li className=' flex gap-3 justify-start btn  text-left items-center'>
+              <HiUserGroup />
+              <NavLink to='reservation'>Reservation</NavLink>
             </li>
-            <li  className=' flex gap-3 justify-start btn btn-info text-left items-center'><BiSolidBarChartAlt2 />
-              <NavLink>Payment History</NavLink>
+            <li className=' flex gap-3 justify-start btn  text-left items-center'><BiSolidBarChartAlt2 />
+              <NavLink to="/payment">Payment History</NavLink>
             </li>
-            <li  className=' flex gap-3 justify-start btn btn-info text-left items-center'><FaCartArrowDown />
-              <NavLink>My Cart</NavLink>
+            <li className=' flex gap-3 justify-start btn  text-left items-center'><FaCartArrowDown />
+              <NavLink to="/mycart">My Cart</NavLink>
             </li>
-            <li  className=' flex gap-3 justify-start btn btn-info text-left items-center'><FaSwatchbook />
-              <NavLink>My Booking</NavLink>
+            <li className=' flex gap-3 justify-start btn  text-left items-center'><FaSwatchbook />
+              <NavLink to="/booking">My Booking</NavLink>
             </li>
           </ul>
-          <div className="divider divider-start">Start</div>
+          <div className="divider divider-start"></div>
           <ul className='  flex flex-col gap-3'>
-            <li  className=' flex gap-3 justify-start btn btn-info text-left items-center'><IoHomeSharp />
-              <NavLink>Home</NavLink>
+            <li className=' flex gap-3 justify-start btn  text-left items-center'><IoHomeSharp />
+              <NavLink to="/adminhome">Home</NavLink>
             </li>
-            <li  className=' flex gap-3 justify-start btn btn-info text-left items-center'><FaBars />
-              <NavLink>Menu</NavLink>
+            <li className=' flex gap-3 justify-start btn  text-left items-center'><FaBars />
+              <NavLink to="menu">Menu</NavLink>
             </li>
-            <li  className=' flex gap-3 justify-start btn btn-info text-left items-center'>
-            <FaShoppingBag />
-              <NavLink>shop</NavLink>
+            <li className=' flex gap-3 justify-start btn  text-left items-center'>
+              <FaShoppingBag />
+              <NavLink to="shop">Shop</NavLink>
             </li>
-            <li  className=' flex gap-3 justify-start btn btn-info text-left items-center'><GrContactInfo /> <NavLink>Cotact</NavLink></li>
+            <li className=' flex gap-3 justify-start btn  text-left items-center'><MdContactPhone /> <NavLink to="/contact">Cotact</NavLink></li>
           </ul>
         </div>
-        <div className=' flex-1'><Outlet /></div>
+        <div className=' flex-1 items-center py-5 flex flex-col'><Outlet /></div>
       </div>
     </>
   )
